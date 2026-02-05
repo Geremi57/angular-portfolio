cd450c63d09a4a558e0e5bccb2bc1fcd19ad6176
gwanga <wangageremi725@gmail.com>
2026-02-05 18:11:09 +0300

    add components to main app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,16 +5,35 @@ import { HeroSectionComponent } from './hero-section/hero-section';
 import { NavbarComponent } from './nav-bar/nav-bar';
 import { AboutSectionComponent } from './about-section/about-section';
 import { ProjectsSectionComponent } from './projects-section/projects-section';
+import { BlogSectionComponent } from './blog-section/blog-section';
+import { ServicesSectionComponent } from './services-section/services-section';
+import { ContactSectionComponent} from './contact-section/contact-section';
+import { FooterComponent } from './footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroSectionComponent, AboutSectionComponent, ProjectsSectionComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HeroSectionComponent,
+    AboutSectionComponent,
+    ProjectsSectionComponent,
+    BlogSectionComponent,
+    ServicesSectionComponent,
+    ContactSectionComponent,
+    FooterComponent
+],
   template: `
     <div class="p-4 bg-black">
       <app-navbar></app-navbar>
       <app-hero-section></app-hero-section>
       <app-about-section></app-about-section>
       <app-projects-section></app-projects-section>
+      <app-blog-section></app-blog-section>
+      <app-services-section></app-services-section>
+      <app-contact-section></app-contact-section>
+      <app-footer></app-footer>
       <router-outlet></router-outlet>
     </div>
   `,
