b8ae0ea27f3b5dbf77b8cc0506d150f11cbd8b46
gwanga <wangageremi725@gmail.com>
2026-02-05 18:10:19 +0300

    add routes for components for easy navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { HeroSectionComponent } from './hero-section/hero-section';
 import { NavbarComponent } from './nav-bar/nav-bar';
 import { AboutSectionComponent } from './about-section/about-section';
 import { ProjectsSectionComponent } from './projects-section/projects-section';
+import { BlogSectionComponent } from './blog-section/blog-section';
+import { ServicesSectionComponent } from './services-section/services-section';
+import { ContactSectionComponent } from './contact-section/contact-section';
+import { FooterComponent } from './footer/footer';
 
 export const routes: Routes = [
   {
@@ -18,11 +22,31 @@ export const routes: Routes = [
   {
     path: 'about-section',
     component: AboutSectionComponent,
-    title: 'about section'
+    title: 'about section',
   },
   {
     path: 'projects-section',
     component: ProjectsSectionComponent,
-    title: 'project sections'
+    title: 'project sections',
+  },
+  {
+    path: 'blog-section',
+    component: BlogSectionComponent,
+    title: 'blog section',
+  },
+  {
+    path: 'services-section',
+    component: ServicesSectionComponent,
+    title: 'services section',
+  },
+  {
+    path: 'contact-section',
+    component: ContactSectionComponent,
+    title: 'contact section',
+  },
+  {
+    path: 'footer section',
+    component: FooterComponent,
+    title: 'footer section'
   }
 ];
