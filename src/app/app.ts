6c25c8328781ed9186e8431ae32f3db197171043
Geremi57 <geremiwanga57@gmail.com>
2026-02-05 10:38:03 +0300

    add imports for project section and use it in the main app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,15 +4,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { HeroSectionComponent } from './hero-section/hero-section';
 import { NavbarComponent } from './nav-bar/nav-bar';
 import { AboutSectionComponent } from './about-section/about-section';
+import { ProjectsSectionComponent } from './projects-section/projects-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroSectionComponent, AboutSectionComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroSectionComponent, AboutSectionComponent, ProjectsSectionComponent],
   template: `
     <div class="p-4 bg-black">
       <app-navbar></app-navbar>
       <app-hero-section></app-hero-section>
       <app-about-section></app-about-section>
+      <app-projects-section></app-projects-section>
       <router-outlet></router-outlet>
     </div>
   `,
