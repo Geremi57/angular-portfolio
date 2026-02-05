9737d315b895d214c1fc353861eef3c8d5294d57
Geremi57 <geremiwanga57@gmail.com>
2026-02-05 10:39:56 +0300

    add route for project component including path ad import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section';
 import { NavbarComponent } from './nav-bar/nav-bar';
 import { AboutSectionComponent } from './about-section/about-section';
+import { ProjectsSectionComponent } from './projects-section/projects-section';
+
 export const routes: Routes = [
   {
     path: 'hero-section',
@@ -17,5 +19,10 @@ export const routes: Routes = [
     path: 'about-section',
     component: AboutSectionComponent,
     title: 'about section'
+  },
+  {
+    path: 'projects-section',
+    component: ProjectsSectionComponent,
+    title: 'project sections'
   }
 ];
